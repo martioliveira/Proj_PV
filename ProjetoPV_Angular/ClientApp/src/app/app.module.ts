e0c75a14450fce271584f198b1d66464fbf24b9c
diogoasp97 <diogoasp97@gmail.com>
2022-04-15 10:48:32 +0100

    Remove template trash
    Remove template trash

--- ProjetoPV_Angular/ClientApp/src/app/app.module.ts
+++ ProjetoPV_Angular/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
@@ -26,8 +24,6 @@ import { ContaEditComponent } from './conta/conta-edit/conta-edit.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ContaListComponent,
     ContaCreateComponent,
     TransacoesListComponent,
@@ -44,8 +40,6 @@ import { ContaEditComponent } from './conta/conta-edit/conta-edit.component';
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
       { path: 'conta-create', component: ContaCreateComponent },
       { path: 'transacoes-create', component: TransacoesCreateComponent },
       { path: 'transacoes-details/:id', component: TransacoesDetailsComponent },
