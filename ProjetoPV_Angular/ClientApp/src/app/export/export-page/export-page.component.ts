c662153fdc6fbd271ae47b99047e6b06a8d9242a
diogoasp97 <diogoasp97@gmail.com>
2022-04-19 14:19:10 +0100

    Visual updates and Export page

--- ProjetoPV_Angular/ClientApp/src/app/export/export-page/export-page.component.ts
+++ ProjetoPV_Angular/ClientApp/src/app/export/export-page/export-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { Conta } from '../../models/conta.model';
 import { ContaService } from '../../models/models-services/conta.service';
+import { TransacaoService } from '../../models/models-services/transacao.service';
+import { Transacao } from '../../models/transacao.model';
 
 @Component({
   selector: 'app-export-page',
@@ -13,60 +15,28 @@ export class ExportPageComponent implements OnInit {
   fileName = 'ProjetoPVExport ' + new Date().toLocaleString() + '.xlsx';
 
   public contas: Conta[] = [];
+  public transacoes: Transacao[] = [];
 
-  userList = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz"
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv"
-    },
-    {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "username": "Samantha",
-      "email": "Nathan@yesenia.net"
-    },
-    {
-      "id": 4,
-      "name": "Patricia Lebsack",
-      "username": "Karianne",
-      "email": "Julianne.OConner@kory.org"
-    },
-    {
-      "id": 5,
-      "name": "Chelsey Dietrich",
-      "username": "Kamren",
-      "email": "Lucio_Hettinger@annie.ca"
-    }
-  ]
-
-  constructor(private contasService: ContaService) { }
+  constructor(private contasService: ContaService, private transacaoService: TransacaoService) { }
 
   ngOnInit(): void {
     this.getContas();
+    this.getTransacaoes();
   }
 
   getContas(): void {
     this.contasService.getContas().subscribe((contas: Conta[]) => this.contas = contas);
   }
 
-  exportAllDummys() {
-    /* pass here the table id */
-    let element = document.getElementById('excel-table');
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+  getTransacaoes(): void {
+    this.transacaoService.getTransacoes().subscribe((transacoes: Transacao[]) => this.transacoes = transacoes);
+  }
 
-    /* generate workbook and add the worksheet */
+  exportAll(tableId: string) {
+    let element = document.getElementById(tableId);
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-    /* save to file */
     XLSX.writeFile(wb, this.fileName);
   }
  
