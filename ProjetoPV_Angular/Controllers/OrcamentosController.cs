b2f8d52f649e6dc3b54321beeea3e13cc3c79d4b
tquaresma <tquaresmadtt@gmail.com>
2022-04-20 23:55:01 +0100

    Novas alterações e aperfeiçoamentos
    Commit de segurança após novas alterações que permitem aceder aos orçamentos por ID e à página de detalhes dos mesmos.
    
    Aperfeiçoamento de outros.

--- ProjetoPV_Angular/Controllers/OrcamentosController.cs
+++ ProjetoPV_Angular/Controllers/OrcamentosController.cs
@@ -24,55 +24,48 @@ namespace ProjetoPV_Angular.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public OrcamentosController(ApplicationDbContext context)
+        public OrcamentosController(ApplicationDbContext context) { _context = context; }
+
+        // GET: api/Orcamentos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Orcamento>>> GetOrcamento()
         {
-            _context = context;
+            return await _context.Orcamento.ToListAsync();
         }
 
-        // GET: api/Orcamentos/Tb2
+        // GET: api/Orcamentos/Tb2 (Versão Custom)
         [HttpGet("Tb2")]
-        public ActionResult<IEnumerable<CustomOrcamento>> GetOrcamentoTb2()
-        {
-            var orcamentoTb1 = _context.Orcamento.ToList();
-
-            var orcamentoConta = orcamentoTb1.Join(_context.OrcamentoContas,
+        public ActionResult<IEnumerable<CustomOrcamento>> GetOrcamentoTb2() {
+            var orcamentoTb1 = _context.Orcamento.ToList();                     
+            // Através da lista de Orçamentos fazer join à tabela de contas
+            var orcamentoConta = orcamentoTb1.Join(_context.OrcamentoContas,    
                                                    orca => orca.OrcamentoId,
                                                    orcc => orcc.OrcamentoId,
                                                    (orca, orcc) => new { orcaId = orcc.OrcamentoId,
                                                                          contId = orcc.ContaId,
                                                                          orcaIn = orca.DataInicio,
                                                                          orcaFi = orca.DataFim});
-            
-            var transacaoConta = orcamentoConta.Join(_context.Transacao.Where(t => t.TipoTransacaoId == 3),
-                                                       orcamentoConta => orcamentoConta.contId,
-                                                       tran           => tran.ContaOrigemId,
-                                                       (orcamentoConta, tran) => new { tranId = tran.TransacaoId,
-                                                                                       tranVa = tran.Valor,
-                                                                                       tranDt = tran.DataTransacao,
-                                                                                       contId = tran.ContaOrigemId,
-                                                                                       orcaId = orcamentoConta.orcaId,
-                                                                                       orcaIn = orcamentoConta.orcaIn,
-                                                                                       orcaFi = orcamentoConta.orcaFi });
 
+            // Após obter o join Orçamentos/Contas obter todas as transações tipo = "Despesas" com um join à tabela de Transações 
+            var transacaoConta = orcamentoConta.Join(_context.Transacao.Where(t => t.TipoTransacaoId == 3), orcamentoConta => orcamentoConta.contId,                   
+                                                                                                            tran           => tran.ContaOrigemId,
+                                                                                                            (orcamentoConta, tran) => new { tranId = tran.TransacaoId,
+                                                                                                                                            tranVa = tran.Valor,
+                                                                                                                                            tranDt = tran.DataTransacao,
+                                                                                                                                            contId = tran.ContaOrigemId,
+                                                                                                                                            orcaId = orcamentoConta.orcaId,
+                                                                                                                                            orcaIn = orcamentoConta.orcaIn,
+                                                                                                                                            orcaFi = orcamentoConta.orcaFi });
+            // Apanhar as transações apenas dentro das datas de Inicio de Fim de Orçamento
             transacaoConta = transacaoConta.Where(t => t.tranDt >= t.orcaIn && t.tranDt < t.orcaFi);
 
-            foreach (var obj in transacaoConta)
-            {
-
-                Console.WriteLine("tranID:{0} - tranVa:{1} - contId:{2} - orcaId:{3}", obj.tranId, obj.tranVa, obj.contId, obj.orcaId);
-            }
-
-            foreach (var obj in orcamentoConta)
-            {
-
-                Console.WriteLine("{0} - {1}", obj.orcaId, obj.contId);
-            }
-
+            // Processar os orçamentos
             var orcamentos = new List<CustomOrcamento>();
             foreach (var c in orcamentoTb1)
             {
+                // Calcular o valor já gasto do orçamento com o somatório de todas as transações
                 var orcamentoGasto = transacaoConta.Where(o => o.orcaId == c.OrcamentoId).Sum(d => d.tranVa);
-
+                // Mapeamento da tabela interna
                 orcamentos.Add(new CustomOrcamento()
                 {
                     OrcamentoId = c.OrcamentoId,
@@ -89,16 +82,7 @@ namespace ProjetoPV_Angular.Controllers
             return orcamentos.ToList();
         }
 
-
-
-        // GET: api/Orcamentoes
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Orcamento>>> GetOrcamento()
-        {
-            return await _context.Orcamento.ToListAsync();
-        }
-
-        // GET: api/Orcamentoes/5
+        // GET: api/Orcamentos/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Orcamento>> GetOrcamento(long id)
         {
@@ -110,6 +94,58 @@ namespace ProjetoPV_Angular.Controllers
             }
 
             return orcamento;
+        }        
+        
+        // GET: api/Orcamentoes/Tb2/5 (Versão Custom)
+        [HttpGet("Tb2/{id}")]
+        public ActionResult<Orcamento> GetOrcamentoId(long id) {
+            // Obter o orçamento pelo Id clicado
+            var orcamento = _context.Orcamento.Find(id);
+
+            if (orcamento == null)
+            {
+                return NotFound();
+            }
+
+            // Através da lista de Orçamento fazer join à tabela de contas onde o Id é o mesmo
+            var orcamentoConta = _context.Orcamento.Where(o => o.OrcamentoId == id).
+                                                     Join(_context.OrcamentoContas,
+                                                           orca => orca.OrcamentoId,
+                                                           orcc => orcc.OrcamentoId,
+                                                           (orca, orcc) => new {
+                                                               orcaId = orcc.OrcamentoId,
+                                                               contId = orcc.ContaId,
+                                                               orcaIn = orca.DataInicio,
+                                                               orcaFi = orca.DataFim     });
+
+            // Após obter o join Orçamentos/Contas obter todas as transações tipo = "Despesas" com um join à tabela de Transações 
+            var transacaoConta = orcamentoConta.Join(_context.Transacao.Where(t => t.TipoTransacaoId == 3),
+                                                       orcamentoConta => orcamentoConta.contId,
+                                                       tran => tran.ContaOrigemId,
+                                                       (orcamentoConta, tran) => new {
+                                                           tranId = tran.TransacaoId,
+                                                           tranVa = tran.Valor,
+                                                           tranDt = tran.DataTransacao,
+                                                           contId = tran.ContaOrigemId,
+                                                           orcaId = orcamentoConta.orcaId,
+                                                           orcaIn = orcamentoConta.orcaIn,
+                                                           orcaFi = orcamentoConta.orcaFi });
+
+            // Apanhar as transações apenas dentro das datas de Inicio de Fim de Orçamento
+            transacaoConta = transacaoConta.Where(t => t.tranDt >= t.orcaIn && t.tranDt < t.orcaFi);
+            // Calcular o valor já gasto do orçamento com o somatório de todas as transações
+            var orcamentoGasto = transacaoConta.Where(o => o.orcaId == orcamento.OrcamentoId).Sum(d => d.tranVa);
+                // Mapeamento da tabela interna
+                var orcamentoCustom = new CustomOrcamento { OrcamentoId = orcamento.OrcamentoId,
+                                                            DataInicio  = orcamento.DataInicio,
+                                                            DataFim     = orcamento.DataFim,
+                                                            Descricao   = orcamento.Descricao,
+                                                            Valor       = orcamento.Valor,
+                                                            Moeda       = orcamento.Moeda,
+                                                            Restante    = orcamento.Valor - orcamentoGasto,
+                                                            Gasto       = orcamentoGasto };
+
+            return orcamentoCustom;
         }
 
         // PUT: api/Orcamentoes/5
