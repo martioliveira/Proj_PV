7fdc11bf036439a1afb48a8c3a268b4687639337
tquaresmadtt <48357499+tquaresmadtt@users.noreply.github.com>
2022-04-20 17:57:03 +0100

    Progresso no desenvolvimento dos Orçamentos (#8)
    * Construção dos Models - Angular
    
    Tradução do C# para Typescript dos models previamente criados.
    
    * Mockups iniciais
    
    Dashboard/Ecrã Principal - Estes ultimos registos vão ter um botão de ver mais da qual irá ser redirecionado para uma página só de registos, tal como quando se clicar no gráfico irá ser redirecionado para uma estatistica e no outro gráfico também. É necessário selecionar a conta correta ou mais que uma para os dados aparecerem com base nisso
    
    Página de Registos (Despesas/Receitas) - Esta página é a que irá conter várias linhas de registos correspondentes a uma conta ou várias. mediante seleção nos botões do topo da página. Terá também um somatório diário/semanal/mensal/anual com base no filtro que mais tarde terá de ser implementado para a exibição dos registos
    
    Página de Planeamento - Esta página permite ter uma visão sucinta dos orçamentos em curso e dos pagamentos planeados para breve, ambas as funcionalidades terão outra página com mais detalhe de cada uma.
    
    * Desenvolvimento dos Orçamentos
    
    Estruturação dos Orçamentos e alguns ajustes adicionais;
    
    * Progresso no desenvolvimento dos Orçamentos
    
    - Desenvolvimento do valor gasto e restante do orçamento;
    - Debate sobre progressos e planos futuros;
    - Algumas correções;

--- ProjetoPV_Angular/ClientApp/src/app/orcamento/orcamento-list/orcamento-list.component.ts
+++ ProjetoPV_Angular/ClientApp/src/app/orcamento/orcamento-list/orcamento-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Orcamento } from '../../models/orcamento.model';
 import { OrcamentoService } from '../../models/models-services/orcamento.service';
 import { Router } from '@angular/router';
+import { OrcamentoTb2 } from '../../models/orcamentotb2.model';
 
 @Component({
   selector: 'app-orcamento-list',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./orcamento-list.component.css']
 })
 export class OrcamentoListComponent implements OnInit {
-  public orcamentos: Orcamento[] = [];
+  public orcamentos: OrcamentoTb2[] = [];
 
   constructor(private service: OrcamentoService,
               private router: Router) { }
@@ -19,14 +20,10 @@ export class OrcamentoListComponent implements OnInit {
   }
 
   getOrcamentos(): void {
-    this.service.getOrcamentos().subscribe((orcamentos: Orcamento[]) => this.orcamentos = orcamentos);
+    this.service.getCustomOrcamentos().subscribe((orcamentos: OrcamentoTb2[]) => this.orcamentos = orcamentos);
   }
 
   onClickOrcamento(id: string) {
     this.router.navigateByUrl('/orcamento-details/' + id);
   }
-
-  //getGastoOrcamento() {
-
-  //}
 }
--- ProjetoPV_Angular/Controllers/OrcamentosController.cs
+++ ProjetoPV_Angular/Controllers/OrcamentosController.cs
@@ -11,6 +11,13 @@ using ProjetoPV_Angular.Models;
 
 namespace ProjetoPV_Angular.Controllers
 {
+    public class CustomOrcamento : Orcamento
+    {
+        public double Restante { get; set; }
+        public double Gasto { get; set; }
+        public CustomOrcamento() : base() { }
+    }
+
     [Route("api/[controller]")]
     [ApiController]
     public class OrcamentosController : ControllerBase
@@ -21,6 +28,68 @@ namespace ProjetoPV_Angular.Controllers
         {
             _context = context;
         }
+
+        // GET: api/Orcamentos/Tb2
+        [HttpGet("Tb2")]
+        public ActionResult<IEnumerable<CustomOrcamento>> GetOrcamentoTb2()
+        {
+            var orcamentoTb1 = _context.Orcamento.ToList();
+
+            var orcamentoConta = orcamentoTb1.Join(_context.OrcamentoContas,
+                                                   orca => orca.OrcamentoId,
+                                                   orcc => orcc.OrcamentoId,
+                                                   (orca, orcc) => new { orcaId = orcc.OrcamentoId,
+                                                                         contId = orcc.ContaId,
+                                                                         orcaIn = orca.DataInicio,
+                                                                         orcaFi = orca.DataFim});
+            
+            var transacaoConta = orcamentoConta.Join(_context.Transacao.Where(t => t.TipoTransacaoId == 3),
+                                                       orcamentoConta => orcamentoConta.contId,
+                                                       tran           => tran.ContaOrigemId,
+                                                       (orcamentoConta, tran) => new { tranId = tran.TransacaoId,
+                                                                                       tranVa = tran.Valor,
+                                                                                       tranDt = tran.DataTransacao,
+                                                                                       contId = tran.ContaOrigemId,
+                                                                                       orcaId = orcamentoConta.orcaId,
+                                                                                       orcaIn = orcamentoConta.orcaIn,
+                                                                                       orcaFi = orcamentoConta.orcaFi });
+
+            transacaoConta = transacaoConta.Where(t => t.tranDt >= t.orcaIn && t.tranDt < t.orcaFi);
+
+            foreach (var obj in transacaoConta)
+            {
+
+                Console.WriteLine("tranID:{0} - tranVa:{1} - contId:{2} - orcaId:{3}", obj.tranId, obj.tranVa, obj.contId, obj.orcaId);
+            }
+
+            foreach (var obj in orcamentoConta)
+            {
+
+                Console.WriteLine("{0} - {1}", obj.orcaId, obj.contId);
+            }
+
+            var orcamentos = new List<CustomOrcamento>();
+            foreach (var c in orcamentoTb1)
+            {
+                var orcamentoGasto = transacaoConta.Where(o => o.orcaId == c.OrcamentoId).Sum(d => d.tranVa);
+
+                orcamentos.Add(new CustomOrcamento()
+                {
+                    OrcamentoId = c.OrcamentoId,
+                    DataInicio  = c.DataInicio,
+                    DataFim     = c.DataFim,
+                    Descricao   = c.Descricao,
+                    Valor       = c.Valor,
+                    Moeda       = c.Moeda,
+                    Restante    = c.Valor - orcamentoGasto,
+                    Gasto       = orcamentoGasto,
+                });
+            }
+
+            return orcamentos.ToList();
+        }
+
+
 
         // GET: api/Orcamentoes
         [HttpGet]
