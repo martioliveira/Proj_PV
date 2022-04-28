3b18ff1a7a347fab7c802ee0ed703137d9aeee7a
martioliveira <martimolivetree@hotmail.com>
2022-04-28 12:30:33 +0100

    Adicionar filtros no GET transações

--- ProjetoPV_Angular/Controllers/TransacaosController.cs
+++ ProjetoPV_Angular/Controllers/TransacaosController.cs
@@ -24,9 +24,57 @@ namespace ProjetoPV_Angular.Controllers
 
         // GET: api/Transacaos
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Transacao>>> GetTransacao()
+        public async Task<ActionResult<IEnumerable<Transacao>>> GetTransacoes([FromQuery] string ContaId,
+            [FromQuery] string TipoTransacaoId, [FromQuery] string CategoriaId, [FromQuery] string DataInicio, [FromQuery] string DataFim)
         {
-            return await _context.Transacao.Include("TipoTransacao").Include(t=>t.Categoria).ToListAsync();
+            var query = _context.Transacao.AsQueryable();
+
+            // Filtrar por conta
+            if (!string.IsNullOrEmpty(ContaId))
+            {
+                if (long.TryParse(ContaId, out var convertedContaId))
+                {
+                    query = query.Where(t => t.ContaOrigemId == convertedContaId || t.ContaDestinoId == convertedContaId);
+                }
+            }
+
+            // Filtrar por tipo de transação
+            if (!string.IsNullOrEmpty(TipoTransacaoId))
+            {
+                if (long.TryParse(TipoTransacaoId, out var convertedTipoTransacaoId))
+                {
+                    query = query.Where(t => t.TipoTransacaoId == convertedTipoTransacaoId);
+                }
+            }
+
+            // Filtrar por categoria
+            if (!string.IsNullOrEmpty(CategoriaId))
+            {
+                if (long.TryParse(CategoriaId, out var convertedCategoriaId))
+                {
+                    query = query.Where(t => t.CategoriaId == convertedCategoriaId);
+                }
+            }
+
+            // Filtrar por data início
+            if (!string.IsNullOrEmpty(DataInicio))
+            {
+                if (DateTime.TryParse(DataInicio, out var convertedDataInicio))
+                {
+                    query = query.Where(t => t.DataTransacao >= convertedDataInicio);
+                }
+            }
+
+            // Filtrar por data fim
+            if (!string.IsNullOrEmpty(DataFim))
+            {
+                if (DateTime.TryParse(DataFim, out var convertedDataFim))
+                {
+                    query = query.Where(t => t.DataTransacao <= convertedDataFim);
+                }
+            }
+
+            return await query.Include(t => t.TipoTransacao).Include(t => t.Categoria).ToListAsync();
         }
 
         // GET: api/Transacaos/5
